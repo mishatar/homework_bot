524efc840abc2dc356af3fe068e89a1c8ec02f75
Mikhail <mishatar2004@gmail.com>
2022-12-18 21:05:55 +0300

    project_sprint7

--- homework.py
+++ homework.py
@@ -5,8 +5,8 @@ from http import HTTPStatus
 
 import requests
 import telegram
-import telegram.ext
 from dotenv import load_dotenv
+from json.decoder import JSONDecodeError
 
 load_dotenv()
 
@@ -15,12 +15,8 @@ PRACTICUM_TOKEN = os.getenv('YA_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('chat_id')
 
-logging.basicConfig(
-    format='%(asctime)s - %(levelname)s - %(message)s - %(name)s',
-    level=logging.INFO)
-
 RETRY_PERIOD = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
+ENDPOINT = os.getenv('ENDPOINT')
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 
@@ -41,9 +37,8 @@ def send_message(bot, message):
     try:
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
         logging.debug(f"Сообщение {message} отправлено")
-    except Exception:
+    except telegram.TelegramError:
         logging.error('Не удалось отправить сообщение')
-        raise Exception('Не удалось отправить сообщение')
 
 
 def get_api_answer(timestamp):
@@ -53,13 +48,15 @@ def get_api_answer(timestamp):
         homework_statuses = requests.get(
             ENDPOINT, headers=HEADERS, params=payload
         )
+        if homework_statuses.status_code != HTTPStatus.OK:
+            raise requests.exceptions.StatusCodeException(
+                'Неверный код ответа API'
+            )
+        return homework_statuses.json()
     except requests.exceptions.RequestException as ex:
         raise Exception(f"Ошибка при запросе к API: {ex}")
-    if homework_statuses.status_code != HTTPStatus.OK:
-        raise requests.exceptions.StatusCodeException(
-            'Неверный код ответа API'
-        )
-    return homework_statuses.json()
+    except JSONDecodeError:
+        raise Exception('Ошибка преобразования json')
 
 
 def check_response(response):
@@ -68,6 +65,9 @@ def check_response(response):
         raise TypeError('Ответ от API не является словарем')
     if 'homeworks' not in response:
         raise TypeError('Ключ "homeworks" не найден')
+    current_date = response.get('current_date')
+    if not isinstance(current_date, int):
+        raise TypeError('current_date не является числом')
     if not isinstance(response['homeworks'], list):
         raise TypeError('В ключе "homeworks" нет списка')
     homeworks = response.get('homeworks')
@@ -80,8 +80,8 @@ def parse_status(homework):
     """Извлекает информацию о статусе домашней работы."""
     if 'homework_name' not in homework:
         raise KeyError('Отсутсвует ключ "homework_name" в ответе API')
-    homework_name = homework['homework_name']
-    status = homework['status']
+    homework_name = homework.get('homework_name')
+    status = homework.get('status')
     if status not in HOMEWORK_VERDICTS:
         logging.error('Неверный статус домашки')
         raise NameError('Неверный статус домашки')
@@ -113,4 +113,8 @@ def main():
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s - %(name)s',
+        level=logging.INFO
+    )
     main()
