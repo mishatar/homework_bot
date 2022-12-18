bb927690c98697f4510cbf6762e319d89ccf364d
Mikhail <mishatar2004@gmail.com>
2022-12-18 23:11:09 +0300

    project_sprint7

--- homework.py
+++ homework.py
@@ -65,6 +65,8 @@ def check_response(response):
         raise TypeError('Ответ от API не является словарем')
     if 'homeworks' not in response:
         raise TypeError('Ключ "homeworks" не найден')
+    if 'current_date' not in response:
+        raise TypeError('Ключ "current_date" не найден')
     current_date = response.get('current_date')
     if not isinstance(current_date, int):
         raise TypeError('current_date не является числом')
